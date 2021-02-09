d64d2ee5fdeeaf00da94d118cd04209017d24184
charbel <charbel-boumaroun@outlook.com>
2021-02-09 13:32:58 +0200

    fix: cleaning directory

--- testLiveTrader.py
+++ testLiveTrader.py
@@ -10,13 +10,17 @@ def parse_args():
     global crypto
     global timeframe
     global file_name
+    global output_file_name
+
     crypto = "ltc"
     timeframe = "1m"
-    file_name = 'data/ltc1m.csv'
+    file_name = "data/ltc1m.csv"
+    output_file_name = "output_of_test_live_trader.txt"
     parser = argparse.ArgumentParser(description='Live trading bot')
     parser.add_argument('-c', '--crypto', help='Set crypto currency', required=False)
     parser.add_argument('-tf', '--timeframe', help='Set time frame', required=False)
     parser.add_argument('-f', '--file', help='File name', required=False)
+    parser.add_argument('-o', '--output', help='Output txt file', required=False)
     args = vars(parser.parse_args())
     if args['crypto']:
         crypto = args['crypto']
@@ -24,7 +28,8 @@ def parse_args():
         timeframe = args['timeframe']
     if args['file']:
         file_name = args['file']
-
+    if args['output']:
+        output_file_name = args['output']
 ###Initializing data
 
 def initialize_data():
@@ -50,8 +55,8 @@ def initialize_data():
 def run_test():
     idx = 0
     while True:
-        inputval = input("Enter command: ")
-        inputval = inputval.lower()
+        input_val = input("Enter command: ")
+        input_val = input_val.lower()
 
         ##If all data has been processed already
         if(idx > len(candlesticks)):
@@ -59,22 +64,22 @@ def run_test():
             continue
 
         ##Plot
-        if(inputval == "plot"):
+        if(input_val == "plot"):
             strategy.plot()
             continue
 
         ##Stop
-        if(inputval == "s" or inputval == "stop"):
+        if(input_val == "s" or input_val == "stop"):
             print('Done')
             break
 
         ##Print to txt
-        if(inputval =="print"):
-            strategy.print_to_txt("output_of_test_live_trader.txt")
+        if(input_val =="print"):
+            strategy.print_to_txt(output_file_name)
             continue
 
         ##Go through all the data
-        if(inputval == "all"):
+        if(input_val == "all"):
             for i in range(len(candlesticks)):
                 message = candlesticks[idx]
                 strategy.add_message(message)
@@ -82,8 +87,8 @@ def run_test():
             continue
 
         ##Number of messages to process
-        if (inputval.isnumeric()):
-            for i in range(int(inputval)):
+        if (input_val.isnumeric()):
+            for i in range(int(input_val)):
                 message = candlesticks[idx]
                 strategy.add_message(message)
                 idx += 1
